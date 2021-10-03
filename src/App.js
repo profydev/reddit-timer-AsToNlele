8b7b215ecbfcb3e54e02b344fbd9914b4e7dd621
AsToN <theastonofficial@gmail.com>
2021-10-03 02:36:44 +0200

    Created App Skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Info from './components/Info';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <Header />
+      <Hero />
+      <Info />
+      <Footer />
+      <Switch>
+        <Route path="/search" />
+        <Route path="/" />
+      </Switch>
+    </Router>
   );
 }
 
